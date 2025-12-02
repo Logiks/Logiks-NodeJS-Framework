50f9cae2e13ad1e6490e5305ec0e04d8e8fc6e6f
Bismay M <bismay@smartinfologks.com>
2025-12-03 02:17:32 +0530

    Updates and fixes to misc helper

--- api/bootstrap.js
+++ api/bootstrap.js
@@ -117,3 +117,40 @@ module.exports = function(server) {
             });
     }
 }
+
+global.printObj = function(msg, clr, intent) {
+  if(intent==null) intent = 2;
+  if(clr==null) clr = "-";
+
+  var newMsg = "";
+  for (let k = 0; k < intent; k++) {
+      newMsg = ">"+newMsg;
+  }
+  msg = newMsg+" "+msg;
+
+  switch(clr.toLowerCase()) {
+      case "grey":
+          console.log("\x1b[30m%s\x1b[0m",`${msg}`);
+          break;
+      case "red":
+          console.log("\x1b[31m%s\x1b[0m",`${msg}`);
+          break;
+      case "green":
+          console.log("\x1b[32m%s\x1b[0m",`${msg}`);
+          break;
+      case "yellow":
+          console.log("\x1b[33m%s\x1b[0m",`${msg}`);
+          break;
+      case "blue":
+          console.log("\x1b[34m%s\x1b[0m",`${msg}`);
+          break;
+      case "pink":
+          console.log("\x1b[35m%s\x1b[0m",`${msg}`);
+          break;
+      case "sky":
+          console.log("\x1b[36m%s\x1b[0m",`${msg}`);
+          break;
+      default:
+          console.log(msg);
+  }
+}
