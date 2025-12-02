6ad5ccbae56dbf05e686486f0e9a0c68d85410ca
Bismay M <bismay@smartinfologks.com>
2025-12-03 02:17:02 +0530

    Updates and fixes to misc helper

--- api/helpers/misc.js
+++ api/helpers/misc.js
@@ -6,85 +6,64 @@ const crypto = require('crypto');
 
 module.exports = function(server) {
 
-  initialize = function() {
-    server.slugify = function(text) {
-      return text.toString().toLowerCase()
-        .replace(/\s+/g, '-')           // Replace spaces with -
-        .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-        .replace(/\-\-+/g, '-')         // Replace multiple - with single -
-        .replace(/^-+/, '')             // Trim - from start of text
-        .replace(/-+$/, '');            // Trim - from end of text
-    }
-  
-    server.toTitle = function(str) {
-      return str.toLowerCase().replace(/\b[a-z]/g, function(letter) {
-          return letter.toUpperCase();
-        });
-    }
-  
-    server.urlify = function(jsonObject) {
-      let urlParameters = Object.entries(jsonObject).map(e => e.join('=')).join('&');
-      return urlParameters;
-    }
-  
-    server.startRequestLog = function() {
-        server.use(function(req, res, next) {
-            if(req.userAgent()=="ELB-HealthChecker/2.0") {
-                return next();
-            }
-            console.log("REQUEST", {
-                    "PATH":req.path(),
-                    "QUERY": req.getQuery(),
-                    "BODY": req.body,
-                    "QUERY": req.query,
-                    "PARAMS": req.params,
-                    // "USER_AGENT": req.userAgent(),
-                    // "HOST": req.header("host"),
-                    // "CLIENT_IP": req.header("x-forwarded-for")
-                });
-            return next();
-        });
-    }
-      
-    server.processUpdateQueryFromBody = function(req, tableName, whereCond, extraFields = "edited_on=?") {
-      var dated = moment().format("Y-M-D HH:mm:ss");
-      
-      var strUpdate = [];
-      _.each(req.body, function(a,b) {
-        strUpdate.push(b+'=?');
+  slugify = function(text) {
+    return text.toString().toLowerCase()
+      .replace(/\s+/g, '-')           // Replace spaces with -
+      .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+      .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+      .replace(/^-+/, '')             // Trim - from start of text
+      .replace(/-+$/, '');            // Trim - from end of text
+  }
+
+  toTitle = function(str) {
+    return str.toLowerCase().replace(/\b[a-z]/g, function(letter) {
+        return letter.toUpperCase();
       });
-      var strSQL = "UPDATE "+tableName+" SET "+strUpdate.join(", ")+","+extraFields+" WHERE "+whereCond;
-      
-      var dataValues = Object.values(req.body);
-      
-      if(extraFields == "edited_on=?") {
-        dataValues.push(dated);
-      }
-      
-      return {
-        "sql" : strSQL,
-        "data" : dataValues
+  }
+
+  urlify = function(jsonObject) {
+    let urlParameters = Object.entries(jsonObject).map(e => e.join('=')).join('&');
+    return urlParameters;
+  }
+    
+  processUpdateQueryFromBody = function(req, tableName, whereCond, extraFields = "edited_on=?") {
+    var dated = moment().format("Y-M-D HH:mm:ss");
+    
+    var strUpdate = [];
+    _.each(req.body, function(a,b) {
+      strUpdate.push(b+'=?');
+    });
+    var strSQL = "UPDATE "+tableName+" SET "+strUpdate.join(", ")+","+extraFields+" WHERE "+whereCond;
+    
+    var dataValues = Object.values(req.body);
+    
+    if(extraFields == "edited_on=?") {
+      dataValues.push(dated);
+    }
+    
+    return {
+      "sql" : strSQL,
+      "data" : dataValues
+    };
+  }
+
+  getDebugInfo = function(req, res) {
+    return {
+        "RUNNING_SINCE":moment(server.config.START_TIME).fromNow(),
+        "DEBUG": CONFIG.debug,
+        "AUDIT": CONFIG.audit,
+        "PATH": req.path(),
+        "URL": req.href(),
+        "QUERY": req.getQuery(),
+        "BODY": req.body,
+        "QUERY": req.query,
+        "PARAMS": req.params,
+        "HEADERS": req.headers,
+        "GUID":req.get("GUID"),
+        // "DEVID":req.body.devid,
+
+        // "CONNECT": CONNECTPARAMS
       };
-    }
-  
-    server.getDebugInfo = function(req, res) {
-      return {
-          "RUNNING_SINCE":moment(server.config.START_TIME).fromNow(),
-          "DEBUG": CONFIG.debug,
-          "AUDIT": CONFIG.audit,
-          "PATH": req.path(),
-          "URL": req.href(),
-          "QUERY": req.getQuery(),
-          "BODY": req.body,
-          "QUERY": req.query,
-          "PARAMS": req.params,
-          "HEADERS": req.headers,
-          "GUID":req.get("GUID"),
-          // "DEVID":req.body.devid,
-  
-          // "CONNECT": CONNECTPARAMS
-        };
-    }
   }
 
   generateUUID = function(prefix,n) {
@@ -151,43 +130,6 @@ module.exports = function(server) {
   }
 
   return this;
-}
-
-global.printObj = function(msg, clr, intent) {
-  if(intent==null) intent = 2;
-  if(clr==null) clr = "-";
-
-  var newMsg = "";
-  for (let k = 0; k < intent; k++) {
-      newMsg = ">"+newMsg;
-  }
-  msg = newMsg+" "+msg;
-
-  switch(clr.toLowerCase()) {
-      case "grey":
-          console.log("\x1b[30m%s\x1b[0m",`${msg}`);
-          break;
-      case "red":
-          console.log("\x1b[31m%s\x1b[0m",`${msg}`);
-          break;
-      case "green":
-          console.log("\x1b[32m%s\x1b[0m",`${msg}`);
-          break;
-      case "yellow":
-          console.log("\x1b[33m%s\x1b[0m",`${msg}`);
-          break;
-      case "blue":
-          console.log("\x1b[34m%s\x1b[0m",`${msg}`);
-          break;
-      case "pink":
-          console.log("\x1b[35m%s\x1b[0m",`${msg}`);
-          break;
-      case "sky":
-          console.log("\x1b[36m%s\x1b[0m",`${msg}`);
-          break;
-      default:
-          console.log(msg);
-  }
 }
 
 global.validateRule = function(formData, ruleObj) {
